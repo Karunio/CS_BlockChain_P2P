8e64665be6cf7ffff8d3eae0820992e4d3a0c886
Karunio <sa_mang@naver.com>
2019-05-04 11:59:59 +0900

    Chang to lamda

--- HashTools.cs
+++ HashTools.cs
@@ -5,28 +5,13 @@ static class HashTools
 {
     private static SHA256 sha256 = SHA256.Create();
 
-    public static byte[] ToBytesHash(byte[] inputBytes)
-    {
-        return sha256.ComputeHash(inputBytes);
-    }
+    public static byte[] ToBytesHash(byte[] inputBytes) => sha256.ComputeHash(inputBytes);
 
-    public static byte[] ToBytesHash(String inputString)
-    {
-        return sha256.ComputeHash(Encoding.UTF8.GetBytes(inputString));
-    }
+    public static byte[] ToBytesHash(String inputString) => sha256.ComputeHash(Encoding.UTF8.GetBytes(inputString));
 
-    public static String ToBase64Hash(byte[] inputBytes)
-    {
-        return Convert.ToBase64String(ToBytesHash(inputBytes));
-    }
+    public static String ToBase64Hash(byte[] inputBytes) => Convert.ToBase64String(ToBytesHash(inputBytes));
 
-    public static String ToBase64Hash(String inputString)
-    {
-        return Convert.ToBase64String(ToBytesHash(inputString));
-    }
-    
-    public static byte[] FromBase64Hash(String inputHash)
-    {
-        return Convert.FromBase64String(inputHash);
-    }
+    public static String ToBase64Hash(String inputString) => Convert.ToBase64String(ToBytesHash(inputString));
+
+    public static byte[] FromBase64Hash(String inputHash) => Convert.FromBase64String(inputHash);
 }
